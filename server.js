c5d57f134c0a4611a75ff601926f2c1c2f5900a4
codez <manishm9730@gmail.com>
2023-06-12 07:19:34 +0530

    Added User Model and basic setup

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express"
+import dotenv from "dotenv"
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import {authRoutes} from "./routes/authRoute.js"
+
+// env configured
+dotenv.config();
+
+// database configured
+connectDB();
+
+const app = express()
+const port = 3000
+
+// middlewares
+app.use(express.json()); // Can pass JSON in req and res
+app.use(morgan('dev'));  // Shows API calls when fn is running
+
+// rest api's
+app.get('/', (req, res) => {
+  res.send('Hello World 123!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
